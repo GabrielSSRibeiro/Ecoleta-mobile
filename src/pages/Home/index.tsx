616f0ac2486589ed21fc1380692784924033e388
Gabriel Ribeiro <49738156+GabrielSSRibeiro@users.noreply.github.com>
2020-06-11 10:05:44 -0400

    Add files via upload

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { Feather as Icon } from "@expo/vector-icons";
+import {
+  View,
+  ImageBackground,
+  Text,
+  Image,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+
+import styles from "./styles";
+
+const Home = () => {
+  const [uf, setUf] = useState("");
+  const [city, setCity] = useState("");
+
+  const navigation = useNavigation();
+
+  function HandleNavigateToPoints() {
+    navigation.navigate("Points", { uf, city });
+  }
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+    >
+      <ImageBackground
+        source={require("../../assets/home-background.png")}
+        style={styles.container}
+        imageStyle={{ width: 274, height: 368 }}
+      >
+        <View style={styles.main}>
+          <Image source={require("../../assets/logo.png")} />
+          <Text style={styles.title}>Seu marketplace de coleta de resíduos.</Text>
+          <Text style={styles.description}>
+            Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+          </Text>
+        </View>
+
+        <View style={styles.footer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Digite a UF"
+            value={uf}
+            maxLength={2}
+            autoCapitalize="characters"
+            autoCorrect={false}
+            onChangeText={setUf}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Digite a cidade"
+            value={city}
+            autoCorrect={false}
+            onChangeText={(text) => setCity(text)}
+          />
+
+          <RectButton style={styles.button} onPress={HandleNavigateToPoints}>
+            <View style={styles.buttonIcon}>
+              <Text>
+                <Icon name="arrow-right" color="#fff" size={24} />
+              </Text>
+            </View>
+            <Text style={styles.buttonText}>Entrar</Text>
+          </RectButton>
+        </View>
+      </ImageBackground>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default Home;
